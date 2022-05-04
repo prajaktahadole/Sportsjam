50d25dc643b09bd603a617dc3752d14155a66af6
prajaktahadole <prajaktahadole@gmail.com>
2022-05-05 03:50:18 +0530

    Update App.js

--- src/App.js
+++ src/App.js
@@ -1,11 +1,12 @@
 import "./App.css";
-// import { Landing } from "./components/LandingHandler/LandingHandler";
+ import { Landing } from "./components/LandingHandler/LandingHandler";
 import { Routing } from "./NavRoutes/Routes.js";
 
 function App() {
   return (
     <div className="App">
       <Routing></Routing>
+      <Landing></Landing>
     </div>
   );
 }

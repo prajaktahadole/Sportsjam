c252d4c843b744fba4c7967dd4866ea1008460be
Shubham Dubey <shubhamdubey.dubey4@gmail.com>
2022-05-04 07:07:59 +0530

    NavBarRoutesHandler

---
+++ src/NavRoutes/Routes.js
@@ -0,0 +1,23 @@
+import { Routes, Route } from "react-router-dom";
+import { Accessories } from "./components/Products/Accessories";
+import { Brands } from "./components/Products/Brands";
+import { Clothing } from "./components/Products/Clothing";
+import { Fangear } from "./components/Products/Fangear";
+import { Fitness } from "./components/Products/Fitness";
+import { LeisureSports } from "./components/Products/LeisureSports";
+import { Shoes } from "./components/Products/Shoes";
+import { Sports } from "@mui/icons-material";
+
+<Routes>
+  <Route path="/Accessories" element={<Accessories></Accessories>}></Route>
+  <Route path="/Brands" element={<Brands></Brands>}></Route>
+  <Route path="/Clothing" element={<Clothing></Clothing>}></Route>
+  <Route path="/Fangear" element={<Fangear></Fangear>}></Route>
+  <Route path="/Fitness" element={<Fitness></Fitness>}></Route>
+  <Route
+    path="/LeisureSports"
+    element={<LeisureSports></LeisureSports>}
+  ></Route>
+  <Route path="/Shoes" element={<Shoes></Shoes>}></Route>
+  <Route path="/Sports" element={<Sports></Sports>}></Route>
+</Routes>;

6dcddcc8a6e5bd586a79152af5da044d6a68d7b8
prajaktahadole <prajaktahadole@gmail.com>
2022-05-06 15:57:33 +0530

    done

--- src/components/LandingHandler/LandingHandler.jsx
+++ src/components/LandingHandler/LandingHandler.jsx
@@ -2,8 +2,36 @@ import {Carousel1} from '../Carousel/carousel'
 //import {Carousel2} from '../Carousel/carousel2'
 import {Carousel3 }from '../Carousel/carousel3'
 import './Landing.css';
+import {Footer} from '../footer/footer'
 
 export const Landing = ()=>{
+
+
+  const div1data = [
+    {image : "https://storage.sg.content-cdn.io/in-resources/b368029c-a4dd-448a-a888-58348cb1b144/Images/userimages/callout_img1.jpg",
+    title : "SPORTS",
+    para : "A huge collection of running shoes and sports equipments",
+    b1 : "BADMINTON",
+    b2 : "FOOTBALL",
+    b3 : "TENNIS",
+  },
+  {image : "https://storage.sg.content-cdn.io/in-resources/b368029c-a4dd-448a-a888-58348cb1b144/Images/userimages/callout_img2.jpg" ,
+    title : "FITNESS",
+    para : "A large selection of gym shoes & gym wear designed to keep you cool and dry whilst looking good in the gym.",
+    b1 : "GYM ACCESSORIES",
+    b2 : "RUNNING",
+    b3 : "STRENGTH TRAINING",
+  },
+  {image : "https://storage.sg.content-cdn.io/in-resources/b368029c-a4dd-448a-a888-58348cb1b144/Images/userimages/callout_img3.jpg",
+    title : "SHOES",
+    para : "Looking for Hiking Boots, Walking Shoes, Sandals or Outdoor Wear? Choose from a large range today.",
+    b1 : "SHOES-SQUASH",
+    b2 : "FOOTBALL",
+    b3 : "RUNNING",
+  }
+  ]
+
+
 
   const div3data = [
     {image : "https://storage.sg.content-cdn.io/in-resources/b368029c-a4dd-448a-a888-58348cb1b144/Images/userimages/victor-offer.jpg" , title : "VICTOR SHOES"},
@@ -15,7 +43,26 @@ export const Landing = ()=>{
     <div className='homeDivMain'>
        <Carousel1></Carousel1>
 
-    <div className='homeDiv1'>div1</div>
+    <div className='homeDiv1 overlay-sec'>
+
+    {div1data.map((e) => (
+              <div className='overlay-img-box'>
+                <img src={e.image} alt=""></img>
+                
+                <div className="img-overlay">
+                <h5 className="img-title">{e.title}</h5>
+                <p className="overlay-img-desc">{e.para}</p>
+                <button>{e.b1}</button>
+                <button>{e.b2}</button>
+                <button>{e.b3}</button>
+                </div>
+              
+              </div>
+            ))}
+
+
+
+    </div>
 
 
 
@@ -73,18 +120,26 @@ export const Landing = ()=>{
     </div>
 
 
-
-
     <div className='homeDiv7'>
       <Carousel3 ></Carousel3>
     </div>
 
 
-
-
-
     <div className='homeDiv8'>
-      footer
+    <h2>Testimonials</h2>
+    <p>Amazing products and Excellent Service. Whenever I have called, I have got professional, prompt and consistent
+        reply. Even during giving the free tshirt, you guys called to know the size, but unfortunately the size is large
+        on me. Any chances I can exchange with M size. Although I am loving my new racquet. - <b>Rajnish, New Delhi</b>
+    </p>
+    <a class="btn" href="/pages/testimonials/pgid-1304966.aspx">View All</a>
+        
+    </div>
+
+
+
+
+    <div className='homeDiv9'>
+      <Footer></Footer>
     </div>
 
 

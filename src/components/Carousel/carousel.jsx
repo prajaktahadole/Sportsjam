ef6e41cff631c5ab5f2a372ecf6c2863cb8d3359
Shubham Dubey <shubhamdubey.dubey4@gmail.com>
2022-05-08 17:50:17 +0530

    done done

--- src/components/Carousel/carousel.jsx
+++ src/components/Carousel/carousel.jsx
@@ -1,56 +1,49 @@
-import 'bootstrap/dist/css/bootstrap.min.css';
-import Carousel from 'react-bootstrap/Carousel'
-// import styles from './carousel.module.css';
+import Carousel from "react-bootstrap/Carousel";
 
-export const Carousel1  = ()=>{
-    return(
-  <div>
-        <Carousel>
+export const Carousel1 = () => {
+  return (
+    <div>
+      <Carousel>
+        <Carousel.Item>
+          <img
+            className="caro-block"
+            src="https://storage.sg.content-cdn.io/in-resources/b368029c-a4dd-448a-a888-58348cb1b144/Images/userimages/Astrox100zz.jpg"
+            alt="First slide"
+          />
+        </Carousel.Item>
 
-                <Carousel.Item>
-                    <img
-                    className="caro-block"
-                    src="https://storage.sg.content-cdn.io/in-resources/b368029c-a4dd-448a-a888-58348cb1b144/Images/userimages/Astrox100zz.jpg"
-                    alt="First slide"
-                    />
-                </Carousel.Item>
-                
-                <Carousel.Item>
-                    <img
-                    className="caro-block"
-                    src="https://storage.sg.content-cdn.io/in-resources/b368029c-a4dd-448a-a888-58348cb1b144/Images/userimages/rafa21.jpg"
-                    alt="Second slide"
-                    />
-                </Carousel.Item>
-                
+        <Carousel.Item>
+          <img
+            className="caro-block"
+            src="https://storage.sg.content-cdn.io/in-resources/b368029c-a4dd-448a-a888-58348cb1b144/Images/userimages/rafa21.jpg"
+            alt="Second slide"
+          />
+        </Carousel.Item>
 
-                <Carousel.Item>
-                    <img
-                    className="caro-block"
-                    src="https://storage.sg.content-cdn.io/in-resources/b368029c-a4dd-448a-a888-58348cb1b144/Images/userimages/Ezone-2022.jpg"
-                    alt="Third slide"
-                    />
-                </Carousel.Item>
+        <Carousel.Item>
+          <img
+            className="caro-block"
+            src="https://storage.sg.content-cdn.io/in-resources/b368029c-a4dd-448a-a888-58348cb1b144/Images/userimages/Ezone-2022.jpg"
+            alt="Third slide"
+          />
+        </Carousel.Item>
 
+        <Carousel.Item>
+          <img
+            className="caro-block"
+            src="https://storage.sg.content-cdn.io/in-resources/b368029c-a4dd-448a-a888-58348cb1b144/Images/userimages/PAANY-R.jpg"
+            alt="Third slide"
+          />
+        </Carousel.Item>
 
-                <Carousel.Item>
-                    <img
-                    className="caro-block"
-                    src="https://storage.sg.content-cdn.io/in-resources/b368029c-a4dd-448a-a888-58348cb1b144/Images/userimages/PAANY-R.jpg"
-                    alt="Third slide"
-                    />
-                </Carousel.Item>
-
-
-                <Carousel.Item>
-                    <img
-                    className="caro-block"
-                    src="https://storage.sg.content-cdn.io/in-resources/b368029c-a4dd-448a-a888-58348cb1b144/Images/userimages/wilson-squash.jpg"
-                    alt="Third slide"
-                    />
-                </Carousel.Item>
-
-       </Carousel>
-</div>
-    )
-}
+        <Carousel.Item>
+          <img
+            className="caro-block"
+            src="https://storage.sg.content-cdn.io/in-resources/b368029c-a4dd-448a-a888-58348cb1b144/Images/userimages/wilson-squash.jpg"
+            alt="Third slide"
+          />
+        </Carousel.Item>
+      </Carousel>
+    </div>
+  );
+};
